d4578fb68efe0519d7a62afc4c8c7707d623c3b2
joseph12n <josephvaron45@gmail.com>
2024-05-07 21:39:32 -0500

    archivos de arranque

---
+++ Controllers/Landing.php
@@ -0,0 +1,8 @@
+<?php
+class Landing{
+    public function __construct(){}
+    public function main(){
+        require_once "views/company/index.view.php";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once "controllers/Landing.php";
+    $controller = new Landing;
+    $controller->main();
+    
+?>
