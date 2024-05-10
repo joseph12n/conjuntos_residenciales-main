b3d3a9ebda69101c6f4fe5b182d34c9cfc9e6161
Juansecm <sebascheshire@gmail.com>
2024-05-09 21:31:51 -0500

    clases (funciones) php

--- Controllers/Users.php
+++ Controllers/Users.php
@@ -1,10 +1,30 @@
 <?php
-require_once "Models/User.php";
+require_once "models/User.php";
 class Users{
-    public function main(){    
+    public function main(){
+        // Objeto Rol
         $rol = new User;
-        $rol->setRolCode("ABC123");
-        echo $rol->getRolName("");
+        $rol->setRolCode("01");
+        echo "Código Rol: ", $rol->getRolCode(), "<hr>";
+        $rol->setRolName("admin");
+        echo "Nombre Rol: ", $rol->getRolName(), "<hr>";
+
+        // Objeto Usuario
+        $user = new User;
+        $user->setUserCode("user_123");
+        echo "Código Usuario: ", $user->getUserCode(), "<hr>";
+        $user->setUserName("Pepito");
+        echo "Nombre Usuario: ", $user->getUserName(), "<hr>";
+        $user->setUserLastName("Perez");
+        echo "Apellido Usuario: ", $user->getUserLastName(), "<hr>";
+        $user->setUserId(123456789);
+        echo "Identificación Usuario: ", $user->getUserId(), "<hr>";
+        $user->setUserEmail("pepito@perez.com");
+        echo "Correo Usuario: ", $user->getUserEmail(), "<hr>";
+        $user->setUserPass(12345);
+        echo "Contraseña Usuario: ", $user->getUserPass(), "<hr>";
+        $user->setUserState(True);
+        echo "Estado Usuario: ", $user->getUserState(), "<hr>";
     }
 }
 ?>
--- Models/User.php
+++ Models/User.php
@@ -11,7 +11,9 @@
         private $user_pass;
         private $user_state;
 
-        // 2da Parte: Setter y Getters
+        // 2da Parte: Sobrecarga Constructores
+
+        // 3ra Parte: Setter y Getters
         # Código Rol
         public function setRolCode($rol_code){
             $this->rol_code = $rol_code;
@@ -19,5 +21,64 @@
         public function getRolCode(){
             return $this->rol_code;
         }
+        # Nombre Rol
+        public function setRolName($rol_name){
+            $this->rol_name = $rol_name;
+        }
+        public function getRolName(){
+            return $this->rol_name;
+        }
+        # Código Usuario
+        public function setUserCode($user_code){
+            $this->user_code = $user_code;
+        }
+        public function getUserCode(){
+            return $this->user_code;
+        }
+        # Nombre Usuario
+        public function setUserName($user_name){
+            $this->user_name = $user_name;
+        }
+        public function getUserName(){
+            return $this->user_name;
+        }
+        # Apellido Usuario
+        public function setUserLastName($user_lastname){
+            $this->user_lastname = $user_lastname;
+        }
+        public function getUserLastName(){
+            return $this->user_lastname;
+        }
+        # Identificación Usuario
+        public function setUserId($user_id){
+            $this->user_id = $user_id;
+        }
+        public function getUserId(){
+            return $this->user_id;
+        }
+        # Email Usuario
+        public function setUserEmail($user_email){
+            $this->user_email = $user_email;
+        }
+        public function getUserEmail(){
+            return $this->user_email;
+        }
+        # Contraseña Usuario
+        public function setUserPass($user_pass){
+            $this->user_pass = $user_pass;
+        }
+        public function getUserPass(){
+            // Método de Encriptación Contraseña: sha1()
+            return sha1($this->user_pass);
+        }
+        # Estado Usuario
+        public function setUserState($user_state){
+            $this->user_state = $user_state;
+        }
+        public function getUserState(){
+            return $this->user_state;
+        }
+
+        // 4ta Parte: Persistencia a la Base de Datos
     }
 ?>
