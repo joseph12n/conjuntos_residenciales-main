9b1ba44453778516e7058f117e063134a789a865
joseph12n <josephvaron45@gmail.com>
2024-08-30 18:18:55 -0500

    test

--- Assets/landing/assets/js/validations.js
+++ Assets/landing/assets/js/validations.js
@@ -1,61 +1,47 @@
-hacerClic = document.getElementById("container");
-hacerClic.addEventListener('click', function (event) {
-    id = event.target.getAttribute("id");
-    if (id === "submit-login") {
-        validar_login();
-    }
+//validacion del login
+var form;
+document.addEventListener('DOMContentLoaded', function() {
+  // Inicializa la variable 'form' cuando el DOM esté listo
+  form = document.forms['form_login'];
+  var submitButton = document.getElementById('submit-login');
+
+  submitButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Previene el comportamiento por defecto del botón de submit
+    validar_login(); // Llama a la función de validación
+  });
 });
 
 function validar_login() {
-    user_email = document.getElementById('user_email').value;
-    user_pass = document.getElementById('user_pass').value;
-    let patron_correo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    event.preventDefault();
-    if (user_email === "") {
-        swal({
-          title: "Verifique el campo correo",
-          text: "El correo NO puede estar vacío",
-          icon: "error",
-          button: "Aceptar",
-        })
-            .then((value) => {
-                document.getElementById('user_email').focus();
-            });
-
-    } else if (!patron_correo.test(user_email)) {
-        event.preventDefault();
-        swal({
-            title: "Verifique el campo email",
-            text: "el email NO puede contener números o caracteres especiales",
-            icon: "error",
-            button: "Aceptar",
-        })
-            .then((value) => {
-                document.getElementById('user_email').focus();
-            });
-    } else if (user_email.length < 5 || user_email.length > 50) {
-        event.preventDefault();
-        swal({
-            title: "Verifique el campo email",
-            text: "La email NO debe contener entre 5 y 50 caracteres",
-            icon: "error",
-            button: "Aceptar",
-        })
-            .then((value) => {
-                document.getElementById('user_email').focus();
-            });
-    } else {
-        swal({
-          title: "Iniciando sesión",
-          text: "Por favor espere...",
-          icon: "info",
-          button: false,
-          timer: 1500
-        }).then((result) => {
-            if (result) {
-                document.form_login.submit();
-            }
-        });
-    }
+  var user_email = document.getElementById('user_email').value;
+  var user_pass = document.getElementById('user_pass').value;
+  event.preventDefault();
+  if (user_email === "") {
+    swal({
+      title: "Verifique el campo correo",
+      text: "El correo NO puede estar vacío",
+      icon: "error",
+      button: "Aceptar",
+    }).then(() => {
+      document.getElementById('user_email').focus();
+    });
+  } else if (user_pass === "") {
+    swal({
+      title: "Verifique el campo contraseña",
+      text: "La contraseña NO puede estar vacía",
+      icon: "error",
+      button: "Aceptar",
+    }).then(() => {
+      document.getElementById('user_pass').focus();
+    });
+  } else {
+    swal({
+      title: "Iniciando sesión",
+      text: "Por favor espere...",
+      icon: "info",
+      button: false,
+      timer: 1500
+    }).then(() => {
+      document.form_login.submit();
+    });
+  }
 }
-
