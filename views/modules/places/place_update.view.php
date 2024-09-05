e30db4ad1bf74e5c91bc7fc53ba1113500a27358
joseph12n <josephvaron45@gmail.com>
2024-09-04 20:03:49 -0500

    validaciones al actualizar

--- views/modules/places/place_update.view.php
+++ views/modules/places/place_update.view.php
@@ -20,16 +20,16 @@
 				</ul>
 			</div>
 			<div class="container-fluid">
-				<form action="" method="POST" class="form-neon" autocomplete="off">
+			<form action="" method="POST" class="form-neon" autocomplete="off" name="form_place_update">
 					<fieldset>
 						<legend><i class="fas fa-user"></i> &nbsp; Actualizar lugar</legend>
 						<div class="container-fluid">
 							<div class="row">
 								<div class="col-12 col-md-6">
-                                <input type="hidden" class="form-control" name="cod_place" id="cod_place" value="<?php echo $placeId->getPlaceCode();?>">
+									<input type="hidden" class="form-control" name="cod_place" id="cod_place" value="<?php echo $placeId->getPlaceCode(); ?>">
 									<div class="form-group">
 										<label for="Placename" class="bmd-label-floating">Nombre lugar</label>
-										<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,40}" class="form-control" name="place_name" id="place_name" maxlength="40" value="<?php echo $placeId->getPlaceName();?>">
+										<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,40}" class="form-control" name="place_name" id="place_update_name" maxlength="40" value="<?php echo $placeId->getPlaceName(); ?>">
 									</div>
 								</div>
 							</div>
@@ -39,7 +39,7 @@
 					<p class="text-center" style="margin-top: 40px;">
 						<button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
 						&nbsp; &nbsp;
-						<button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp; ACTUALIZAR</button>
+						<button type="submit" class="btn btn-raised btn-info btn-sm" id="submit-update-place"><i class="far fa-save"></i> &nbsp; ACTUALIZAR</button>
 					</p>
 				</form>
 			</div>
