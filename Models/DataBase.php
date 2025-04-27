efcfcadde174a63ae8d0706d312cf6ba0a734c3a
joseph12n <josephvaron45@gmail.com>
2025-04-27 16:15:37 -0500

    db reinicio

--- Models/DataBase.php
+++ Models/DataBase.php
@@ -2,31 +2,31 @@
 class DataBase
 {
 
-    public static function connection()
-    {
-        $hostname = "conjuntosreservas.mysql.database.azure.com";
-        $port = "3306";
-        $database = "conjuntos_reservas";
-        $username = "joseph12";
-        $password = "conjuntosR12";
-        $options = array(
-            PDO::MYSQL_ATTR_SSL_CA => 'Assets/DigiCertGlobalRootCA.crt.pem'
-        );
-        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password, $options);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    }
-}
-
 //     public static function connection()
 //     {
-//         $hostname = "localhost";
+//         $hostname = "conjuntosreservas.mysql.database.azure.com";
 //         $port = "3306";
 //         $database = "conjuntos_reservas";
-//         $username = "root";
-//         $password = "";
-//         $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+//         $username = "joseph12";
+//         $password = "conjuntosR12";
+//         $options = array(
+//             PDO::MYSQL_ATTR_SSL_CA => 'Assets/DigiCertGlobalRootCA.crt.pem'
+//         );
+//         $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password, $options);
 //         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //         return $pdo;
 //     }
 // }
+
+    public static function connection()
+    {
+        $hostname = "localhost";
+        $port = "3306";
+        $database = "conjuntos_reservas";
+        $username = "root";
+        $password = "";
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+}
